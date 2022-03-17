20714ff06e610a9d8e5cb1724e54d2cd8279ccfb
“Ezra <ezrapiya.th@gmail.com>
2022-03-18 05:38:10 +0700

    edit redirectUri

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,6 +1,6 @@
 const clientId = 'b52333a1ab894bc59cd98831579f8703';
 
-const redirectUri = 'http://localhost:3000';
+const redirectUri = 'https://add-spotify-playlist.netlify.app';
 const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=token&scope=playlist-modify-public&client_id=${clientId}&redirect_uri=${redirectUri}`;
 let accessToken = undefined;
 let expiresIn = undefined;
