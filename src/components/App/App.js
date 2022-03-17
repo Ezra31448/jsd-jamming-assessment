37123f9510d31dc057f8426d096e51967698d480
“Ezra <ezrapiya.th@gmail.com>
2022-03-17 12:51:46 +0700

    Add HTML Like

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
   return (
     <div>
-      Put components here to make them show up in the browser. Good Luck!
+  <h1>Ja<span class="highlight">mmm</span>ing</h1>
+  <div class="App">
+      <SearchBar />
+      <div class="App-playlist">
+        // Add a SearchResults component 
+        // Add a Playlist component
+      </div>
     </div>
+  </div>
   );
 }
 
